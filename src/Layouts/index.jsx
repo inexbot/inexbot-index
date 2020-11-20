58dfea0742953cd9a558e0416b17f370e577e946
weifan <weifan@inexbot.com>
2020-11-20 10:08:29 +0800

    cc

--- src/Layouts/index.jsx
+++ src/Layouts/index.jsx
@@ -1,41 +1,25 @@
-import React, { useState, useEffect } from "react";
-import API from "API/api";
-import intl from "react-intl-universal";
-import Header from "components/header/index.jsx";
-import style from  "./index.module.less";
-const locales = {
-  "en-US": require("../locales/en-US.json"),
-  "zh-CN": require("../locales/zh-CN.json"),
-  null: require("../locales/zh-CN.json"),
-};
+import React, { useState, useEffect } from 'react';
+import Header from 'components/header/index.jsx';
+import style from './index.module.less';
 
 function App(props) {
   const [BannerWidth, setBannerWidth] = useState(document.body.clientWidth);
-  const [BannerHeight, setBannerHeight] = useState(document.body.clientHeight);
-  const [initDone, setInitDone] = useState(false);
 
-  // useEffect(()=>{
-  //   setBannerWidth(document.body.clientWidth)
-  //   console.log(document.body.clientWidth)
-  // },[document.body.clientWidth])
-  
-  window.onresize = function(){
-    setBannerWidth(document.body.clientWidth)
+  window.onresize = function() {
+    setBannerWidth(document.body.clientWidth);
     // setBannerHeight(document.body.clientHeight)
-
   };
 
   return (
-    initDone && (
-      <div className="App">
-        {/* 主页头部,Header组件*/}
-        <div className={style.Header}>
-          <Header BannerWidth={BannerWidth} />
-        </div>
-        {/* 中间内容 */}
-        <div className="content">{props.children}</div>
+    <div className="App">
+      {/* 主页头部,Header组件*/}
+      <div className={style.Header}>
+        <Header BannerWidth={BannerWidth} />
       </div>
-   ));
+      {/* 中间内容 */}
+      <div className="content">{props.children}</div>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from "react";
-import {Carousel,Button } from "antd";
-import API from "components/API/api";
+import React, { useState, useEffect } from 'react';
+import { Carousel, Button } from 'antd';
+import API from 'components/API/api';
 // import style from "./index.module.less";
-import Banner from "./index/banner/index.jsx";
-import Solution from "./index/solution/index.jsx";
+import Banner from './index/banner/index.jsx';
+import Solution from './index/solution/index.jsx';
 
-function Connect(props){
+function Connect(props) {
   const [BannerWidth, setBannerWidth] = useState(document.body.clientWidth);
   const [BannerHeight, setBannerHeight] = useState(document.body.clientHeight);
 
-//   window.onresize = function(){
-//     setBannerWidth(document.body.clientWidth)
-//     setBannerHeight(document.body.clientHeight)
-//     console.log(props,"这里是子页面的props")
-//   };
-useEffect(()=>{
-    setBannerWidth(document.body.clientWidth)
-    setBannerHeight(document.body.clientHeight)
-  },[document.body.clientWidth,document.body.clientHeight])
+  //   window.onresize = function(){
+  //     setBannerWidth(document.body.clientWidth)
+  //     setBannerHeight(document.body.clientHeight)
+  //     console.log(props,"这里是子页面的props")
+  //   };
+  useEffect(() => {
+    setBannerWidth(document.body.clientWidth);
+    setBannerHeight(document.body.clientHeight);
+  }, [document.body.clientWidth, document.body.clientHeight]);
   return (
     <div>
       {/* banner图 */}
-      <div className="banner"  >
+      <div className="banner">
         <Banner BannerHeight={BannerHeight} BannerWidth={BannerWidth} />
       </div>
       {/* 解决方案 */}
-      <div className="solution"  >
+      <div className="solution">
         <Solution BannerHeight={BannerHeight} BannerWidth={BannerWidth} />
       </div>
     </div>
-  )
+  );
 }
 
 export default Connect;
