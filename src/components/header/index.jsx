4f227e30a5a204212394fd55391aea4c1a188323
weifan <weifan@inexbot.com>
2020-11-24 11:01:50 +0800

    a

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,55 +1,78 @@
-import React, { useState, useEffect } from "react";
-import style from "./index.module.less";
-import {SearchOutlined , } from "@ant-design/icons"
+import React, { useState, useEffect } from 'react';
+import style from './index.module.less';
+import { SearchOutlined } from '@ant-design/icons';
 
-function Header(props){
-  const [TypeList, setTypeList] = useState(props.TypeList)
-
-  useEffect(()=>{
-    if( props.TypeList !== null ){
-      props.TypeList.splice(8)
+function Header(props) {
+  const [TypeList, setTypeList] = useState(props.TypeList);
+  function getLink(link) {
+    let _cms = '{cmspath}';
+    if (link.match(_cms)) {
+      let _linkArray = link.split('}');
+      return _linkArray[1];
+    } else {
+      return link;
     }
-    setTypeList(props.TypeList)
-  },[props.TypeList])
+  }
+  useEffect(() => {
+    if (props.TypeList !== null) {
+      props.TypeList.splice(8);
+    }
+    setTypeList(props.TypeList);
+  }, [props.TypeList]);
   return (
-    <div className={style.header} style={{ width:props.BannerWidth, padding:`0 ${(props.BannerWidth-1280)/2}px` }}>
+    <div
+      className={style.header}
+      style={{
+        width: props.BannerWidth,
+        padding: `0 ${(props.BannerWidth - 1280) / 2}px`,
+      }}
+    >
       <h1>
-        <a>
-          <img src="https://forinexbotweb.oss-cn-shanghai.aliyuncs.com/skin/images/logo.png" ></img>
+        <a href="https://www.inexbot.com">
+          <img src="https://forinexbotweb.oss-cn-shanghai.aliyuncs.com/skin/images/logo.png"></img>
           <span>纳博特科技</span>
         </a>
       </h1>
       <ul className={style.header_list}>
-        { TypeList === null? " " :
-          TypeList.map((Item,Index)=>{
-            return (
-              <li key={Index} >
-                <a  >{Item.typename} <span></span></a>
-                <ul>
-                {Item.sublist.map((item,index)=>{
-                  return(
-                    <li key={index}><a > {item.typename}<span></span> </a> </li>
-                  )
-                })}
-                </ul>
-              </li>
-            )
-          })
-        }
+        {TypeList === null
+          ? ' '
+          : TypeList.map((Item, Index) => {
+              return (
+                <li key={Index}>
+                  <a href={getLink(Item.sitepath)}>
+                    {Item.typename} <span></span>
+                  </a>
+                  <ul>
+                    {Item.sublist.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <a href={getLink(item.typedir)}>
+                            {' '}
+                            {item.typename}
+                            <span></span>{' '}
+                          </a>{' '}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </li>
+              );
+            })}
       </ul>
       <div className={style.header_txt}>
-        <a style={{fontSize: "15px",color:"white"}}> 中文 </a>
-        <span >&nbsp; / &nbsp;</span>
-        <a style={{fontSize:"14px",color:"#a5b3be"}}> EN </a>
+        <a style={{ fontSize: '15px', color: 'white' }}> 中文 </a>
+        <span>&nbsp; / &nbsp;</span>
+        <a style={{ fontSize: '14px', color: '#a5b3be' }}> EN </a>
       </div>
-      <div className={style.top_sousuo} style={{ position:"relative" }}>
-        <a className={style.icon_sousuo} ><SearchOutlined  /></a>
+      <div className={style.top_sousuo} style={{ position: 'relative' }}>
+        <a className={style.icon_sousuo}>
+          <SearchOutlined />
+        </a>
         <input className={style.icon_sousuo_ipt} />
         <span></span>
       </div>
-      
     </div>
-  )
+  );
 }
 
 export default Header;
