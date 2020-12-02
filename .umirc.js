5200b634532d2ac77fc369657b3f2997354719b6
weifan <weifan@inexbot.com>
2020-12-02 18:25:46 +0800

    cc

--- .umirc.js
+++ .umirc.js
@@ -18,7 +18,7 @@ export default defineConfig({
         '纳博特在基于深度学习的人工智能控制、基于三维视觉的环境感知技术、关节型机器人动力学参数辨识、机器人高速高精度轨迹跟踪、机器人无序抓取、机器人力控打磨、机器人拖动示教等人工智能与机器人技术领域拥有核心技术与国家发明专利，发布了基于PC架构的机器人控制器产品，可用于工业机器人和CNC控制、机器视觉自动检测等机器人智能自动化系统，获得数百家客户的成功应用，系统综合性能达到世界领先水平，遥遥领先国内同类产品，填补国内相关行业的多项技术空白。纳博特的产品可广泛应用于弧焊焊接、电子装配、机械加工， 喷涂、码垛、切割、冲压等众多领域。',
     },
   ],
-  publicPath: 'https://forinexbotweb.oss-cn-shanghai.aliyuncs.com/index/',
+  // publicPath: 'https://forinexbotweb.oss-cn-shanghai.aliyuncs.com/index/',
   history: {
     type: 'hash',
   },
