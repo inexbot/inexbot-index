ac10355db06b6f51425d63b3257cbe75feb2eba8
honey_kai <honey_kai@163.com>
2020-12-02 18:15:27 +0800

    增加了服务支持页面的连接跳转

--- src/pages/index/serviceSupport/index.jsx
+++ src/pages/index/serviceSupport/index.jsx
@@ -6,6 +6,16 @@ import { RightCircleOutlined } from '@ant-design/icons';
 function serviceSupport(props){
   const [contentList, setContentList] = useState(null);
 
+  function getLink(link) {
+    let _cms = '{cmspath}';
+    if (link.match(_cms)) {
+      let _linkArray = link.split('}');
+      return _linkArray[1];
+    } else {
+      return link;
+    }
+  }
+
   useEffect(()=>{
     if( props.TypeList === null ){
       return;
@@ -13,6 +23,7 @@ function serviceSupport(props){
     for(let i=0; i<props.TypeList.length; i++){
       if(props.TypeList[i].id === 34){
         setContentList(props.TypeList[i])
+        console.log(props.TypeList[i],"这里是服务支持模块")
       }
     }
   },[props.TypeList])
@@ -29,15 +40,18 @@ function serviceSupport(props){
           <div className="serviceSupport_content">
             { contentList.sublist.map((item,index)=>{
               return (
-                <div key={index}>
-                  <p>{item.typename}</p>
-                  <div>
+                <div key={index}onClick={()=>{
+                  location.href = getLink(item.typedir)
+                }}>
+                  <p >{item.typename}</p>
+                  <div >
                     <img src={require(`images/icon_sp${index+1}.png`)} alt=""/>
                   </div>
                   <div>
                     <span className="serviceSupport_content_bx"></span>
                   </div>
-                  <RightCircleOutlined className="serviceSupport_content_icon" />
+                  <RightCircleOutlined className="serviceSupport_content_icon"/>
+                  <span className="serviceSupport_content_description">{item.description}</span>
                 </div>
               )
             }) }
