a51b62fefaf692dc34dc35322e118f4cff8609fc
honey_kai <honey_kai@163.com>
2020-12-09 11:14:12 +0800

    修改新闻页面

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -7,7 +7,7 @@ function Banner(props){
     console.log(props)
   },[])
   return(
-    <div className={style.BannerAll} style={{ height:`${props.data.Height}px`,background:`url(${props.data.BannerImg}) 0 -200px`,backgroundSize:"100%" }}>
+    <div className={style.BannerAll} style={{ background:`url(${props.data.BannerImg}) 0 -200px`,backgroundSize:"100%" }}>
       <div className={style.model} ></div>
       <div className={style.Banner} >
         <p className={style.banner_txt_ch}> {props.data.TxtCh} </p>
