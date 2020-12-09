5a2a4b6c5c8bc70d137d4bb47d3a5f7a6e269183
weifan <weifan@inexbot.com>
2020-12-09 18:12:42 +0800

    cc

---
+++ src/components/banner/index.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import style from './index.module.less';
+
+function Banner(props) {
+  useEffect(() => {
+    console.log(props);
+  }, []);
+  return (
+    <div
+      className={style.BannerAll}
+      style={{
+        background: `url(${props.data.BannerImg}) 0 -200px`,
+        backgroundSize: '100%',
+      }}
+    >
+      <div className={style.model}></div>
+      <div className={style.Banner}>
+        <p className={style.banner_txt_ch}> {props.data.TxtCh} </p>
+        <p className={style.banner_txt_en}> {props.data.TxtEn} </p>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
